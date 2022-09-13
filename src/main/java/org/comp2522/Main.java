217d73238c6036616b92d2a7210d03fa1d9cf92e
Jason Lee <jlee1473@my.bcit.ca>
2022-09-12 22:57:41 -0700

    Changed default message to "Hello Comp 2522!"

---
+++ src/main/java/org/comp2522/Main.java
@@ -0,0 +1,10 @@
+package org.comp2522;
+
+/**
+ * Program prints a welcome message for Comp 2522.
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello Comp 2522!");
+  }
+}
